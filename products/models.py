cce5ab02a43a94d9024217e6096aa3645b037974
Ayush  Shrestha <aayush625@gmail.com>
2025-11-22 12:04:01 +0545

    product models

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils.text import slugify
+import json
 # Create your models here.
 class Supplier(models.Model):
     name = models.CharField(max_length=255)
@@ -11,3 +12,19 @@ class Supplier(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Product(models.Model):
+    name =models.CharField(max_length=255)
+    slug =models.SlugField(unique=True, max_length=255)
+    category =models.CharField(max_length=255)
+    price_npr = models.DecimalField(max_digits=10, decimal_places=2)
+    landed_cost =models.DecimalField(max_digits=10, decimal_places=2)
+    stock =models.PositiveIntegerField(default=0)
+    supplier = models.ForeignKey(Supplier,on_delete=models.SET_NULL)
+    image = models.ImageField(upload_to='products/')
+
+    #JSON/TEXT field for specifications
+    specs= models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
