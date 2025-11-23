b2643602fe0e21067aeb60b03781311aff5be597
Ayush  Shrestha <aayush625@gmail.com>
2025-11-23 12:07:57 +0545

    inquiry model

--- products/models.py
+++ products/models.py
@@ -53,3 +53,15 @@ class Tender(models.Model):
 
 def __str__(self):
     return self.title
+
+# Inquiry Model
+class Inquiry(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    message = models.TextField()
+    product =models.ForeignKey(Product, on_delete=models.SET_NULL)
+    created_at =models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return f"Inquiry from {self.name}"
