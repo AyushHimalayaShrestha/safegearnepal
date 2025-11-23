4f3241c940202fd2af7fd09c5fe863742b29a1fa
Ayush  Shrestha <aayush625@gmail.com>
2025-11-23 12:12:01 +0545

    registering admin product

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Product, ProductImage
 # Register your models here.
+class ProductImageInLine(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display =('name','category','price', 'created_at')
+    list_filter = ('category','created_at')
+    search_fields = ('name','description')
+    inlines = [ProductImageInLine]
