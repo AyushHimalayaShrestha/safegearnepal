894281b6ecbd7928369c6718141c224d5a2c10a3
Ayush  Shrestha <aayush625@gmail.com>
2025-11-25 09:40:00 +0545

    home template

--- products/views.py
+++ products/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,get_object_or_404
 from .models import Product
 # Create your views here.
 
+def home(request):
+    products =Product.objects.all()[:3]
+    return render(request,"products/home.html",{"products":products})
+
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html',{'products':products})
