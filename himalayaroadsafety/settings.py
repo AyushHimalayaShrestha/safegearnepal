d1adae081c7884b5a62ca2331176bd97e39aac91
Ayush  Shrestha <aayush625@gmail.com>
2025-12-03 13:33:09 +0545

    login template

--- himalayaroadsafety/settings.py
+++ himalayaroadsafety/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = "himalayaroadsafety.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -136,3 +136,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Authentication Settings
+LOGIN_URL = 'accounts:login' 
+LOGIN_REDIRECT_URL = 'products:home' 
+LOGOUT_REDIRECT_URL = 'products:home'
